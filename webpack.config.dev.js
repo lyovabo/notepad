f7b00254628f4944c9f573f34da07a6c7b517cf6
Lyovs <minasyanlevon@gmail.com>
2019-07-27 22:52:37 +0400

    configurations

---
+++ webpack.config.dev.js
@@ -0,0 +1,71 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  devtool: "cheap-module-source-map",
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    stats: "minimal",
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.API_URL": JSON.stringify("http://localhost:3009")
+    }),
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.(css|sass|scss)$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: function() {
+                return [
+                  require("precss"),
+                  require("autoprefixer"),
+                  require("cssnano")
+                ];
+              },
+              sourceMap: true
+            }
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
+  }
+};
